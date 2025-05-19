dd5bb005d29c8550a14d254933e354a1b62db63c
Andreas Geiges <andreas.geiges@climateanalytics.org>
2025-05-19 10:52:17 +0200

    update of mock_snakemake for various snakemake versions

--- snakemake_tools/helper.py
+++ snakemake_tools/helper.py
@@ -68,13 +68,124 @@ def configure_logging(snakemake, skip_handlers=False):
         )
 
     sys.excepthook = handle_exception
+
+def mock_snakemake(rulename, 
+                   root_dir=None,
+                   script_file= None, 
+                   configfiles=[], 
+                   **wildcards):
     
     
-def mock_snakemake(
+    import snakemake as sm
+    
+    if int(sm.__version__[0]) > 8:
+        if root_dir is None:
+            raise(Exception('Please provide parameter "root_dir" when using snakemake version greater than 8.x.x'))
+        return _mock_snakemake_gt_sn8(
+            rulename,
+            root_dir,
+            configfiles,
+            submodule_dir=None,
+            **wildcards,
+        )
+    else:
+        _mock_snakemake_lt_sn8(rulename, script_file, configfiles=[], **wildcards)
+        
+
+def _mock_snakemake_lt_sn8(rulename, script_file, configfiles=[], **wildcards):
+    """
+    # from pypsa-eur
+    This function is expected to be executed from the 'scripts'-directory of '
+    the snakemake project. It returns a snakemake.script.Snakemake object,
+    based on the Snakefile.
+    If a rule has wildcards, you have to specify them in **wildcards.
+    Parameters
+    ----------
+    rulename: str
+        name of the rule for which the snakemake object should be generated
+    configfiles: list, str
+        list of configfiles to be used to update the config
+    **wildcards:
+        keyword arguments fixing the wildcards. Only necessary if wildcards are
+        needed.
+    """
+    import os
+
+    import snakemake as sm
+    from packaging.version import Version, parse
+    from snakemake.script import Snakemake
+
+    script_dir = Path(script_file).parent.resolve()
+    root_dir = script_dir.parent
+
+    user_in_script_dir = Path.cwd().resolve() == script_dir
+    if user_in_script_dir:
+        os.chdir(root_dir)
+    elif Path.cwd().resolve() != root_dir:
+        pass
+        # raise RuntimeError(
+        #     "mock_snakemake has to be run from the repository root"
+        #     f" {root_dir} or scripts directory {script_dir}"
+        # )
+    try:
+        for p in sm.SNAKEFILE_CHOICES:
+            if os.path.exists(p):
+                snakefile = p
+                break
+        kwargs = (
+            dict(rerun_triggers=[]) if parse(sm.__version__) > Version("7.7.0") else {}
+        )
+        if isinstance(configfiles, str):
+            configfiles = [configfiles]
+
+        workflow = sm.Workflow(snakefile, overwrite_configfiles=configfiles, **kwargs)
+        workflow.include(snakefile)
+
+        if configfiles:
+            for f in configfiles:
+                if not os.path.exists(f):
+                    raise FileNotFoundError(f"Config file {f} does not exist.")
+                workflow.configfile(f)
+        
+        workflow.global_resources = {}
+        rule = workflow.get_rule(rulename)
+        dag = sm.dag.DAG(workflow, rules=[rule])
+        wc = wildcards #Dict(wildcards)
+        job = sm.jobs.Job(rule, dag, wc)
+        
+        def make_accessable(*ios):
+                for io in ios:
+                    for i in range(len(io)):
+                        io[i] = os.path.abspath(io[i])
+                    
+
+        make_accessable(job.input, job.output, job.log)
+        snakemake = Snakemake(
+            job.input,
+            job.output,
+            job.params,
+            job.wildcards,
+            job.threads,
+            job.resources,
+            job.log,
+            job.dag.workflow.config,
+            job.rule.name,
+            None,
+        )
+        # create log and output dir if not existent
+        for path in list(snakemake.log) + list(snakemake.output):
+            Path(path).parent.mkdir(parents=True, exist_ok=True)
+
+    finally:
+        if user_in_script_dir:
+            os.chdir(script_dir)
+    return snakemake
+    
+def _mock_snakemake_gt_sn8(
     rulename,
     root_dir=None,
     configfiles=None,
-    submodule_dir="workflow/submodules/pypsa-eur",
+    submodule_dir=None,
     **wildcards,
 ):
     """
@@ -114,10 +225,10 @@ def mock_snakemake(
         StorageSettings,
         WorkflowSettings,
     )
-
+    print(root_dir)
     script_dir = Path(__file__).parent.resolve()
     if root_dir is None:
-        root_dir = script_dir.parent
+        root_dir = script_dir
     else:
         root_dir = Path(root_dir).resolve()
 
@@ -141,6 +252,8 @@ def mock_snakemake(
             if os.path.exists(p):
                 snakefile = p
                 break
+        else:
+            raise(Exception('No snakefile found'))
         if configfiles is None:
             configfiles = []
         elif isinstance(configfiles, str):
@@ -199,6 +312,7 @@ def mock_snakemake(
     finally:
         if user_in_script_dir:
             os.chdir(script_dir)
+        
     return snakemake
 
 def load_config_yaml(configfile):
